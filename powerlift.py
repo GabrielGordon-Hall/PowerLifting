720bb203b04e82be2c7d477a6cff01734c74c82d
GabrielGordon-Hall <ggordo04@dcs.bbk.ac.uk>
2018-03-19 13:23:12 +0000

    first commit

---
+++ powerlift.py
@@ -0,0 +1,20 @@
+import math
+import os
+
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+powerlifting_file = open("powerlifting-database/openpowerlifting.csv")
+powerlifting_dataframe = pd.read_csv(powerlifting_file, sep=",")
+powerlifting_dataframe = powerlifting_dataframe.reindex(
+    np.random.permutation(powerlifting_dataframe.index))

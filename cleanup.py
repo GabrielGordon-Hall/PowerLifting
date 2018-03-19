4a60e48780cf0a6d697c5dfd1ea050423bf3d3ff
GabrielGordon-Hall <ggordo04@dcs.bbk.ac.uk>
2018-03-19 14:22:46 +0000

    'Added cleaned up csv'

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 powerlifting_file = open("powerlifting-database/openpowerlifting.csv", "rb")
@@ -7,10 +6,11 @@ powerlifting_dataframe = pd.read_csv(powerlifting_file, sep=",")
 powerlifting_dataframe = powerlifting_dataframe.drop(["MeetID", "Name", "Division", "WeightClassKg", "Squat4Kg", \
                                                       "Bench4Kg", "Deadlift4Kg", "TotalKg", "Place", "Wilks"], 1)
 powerlifting_dataframe = powerlifting_dataframe.dropna()
-mapping = {'M': 0, 'F': 1}
-powerlifting_dataframe["Sex"]
+sex_mapping = {'M': 0.0, 'F': 1.0}
+equipment_mapping = {'Wraps': 0.0, 'Single-ply': 1.0, 'Raw': 2.0, 'Multi-ply': 3.0}
 
-print(powerlifting_dataframe.info())
+powerlifting_dataframe = powerlifting_dataframe.replace({'Sex': sex_mapping, 'Equipment': equipment_mapping})
+powerlifting_dataframe.to_csv("cleanedlifting.csv", sep=',', encoding='utf-8')
 
 
 
